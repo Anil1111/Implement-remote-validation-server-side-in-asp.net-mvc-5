35bf93fa84c6671ad99b25c678133d875db8ff8d
Jean Lopes <jeean.llopes@outlook.com>
2018-01-14 16:26:54 -0200

    Add Project Add Controller

---
+++ Implement-remote-validation-in-asp.net-mvc-5/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Implement_remote_validation_in_asp.net_mvc_5.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        [Remote("IsUserNameAlreadyExist", "User", ErrorMessage = "Username already exist.")]
+        public string UserName { get; set; }
+        public string EmailId { get; set; }
+
+    }
+}
